1b26a0dd9d271415b82b224b7621e043e6947199
Patrick Porto <patrick.porto@stone.com.br>
2018-01-10 17:32:36 -0200

    Add method for create index

--- kant/db/backends/postgresql.py
+++ kant/db/backends/postgresql.py
@@ -1,6 +1,7 @@
 class DatabaseWrapper(object):
     sql_create_table = 'CREATE TABLE {name}'
     sql_drop_table = 'DROP TABLE {name}'
+    sql_create_index = 'CREATE INDEX {name} ON {table} ({columns})'
 
     def create_table(self, name):
         name = self.escape_string(name)
@@ -9,6 +10,17 @@ class DatabaseWrapper(object):
     def drop_table(self, name):
         name = self.escape_string(name)
         return self.sql_drop_table.format(name=name)
+
+    def create_index(self, name, table, columns):
+        name = self.escape_string(name)
+        table = self.escape_string(table)
+        columns = [self.escape_string(column) for column in columns]
+        stmt = self.sql_create_index.format(
+            name=name,
+            table=table,
+            columns=','.join(columns)
+        )
+        return stmt
 
     def escape_string(self, text):
         return text
--- tests/db/backends/postgresql/test_schema.py
+++ tests/db/backends/postgresql/test_schema.py
@@ -19,3 +19,19 @@ def test_drop_table_should_return_sql_drop_table():
     sql = database_wrapper.drop_table(name=table_name)
     # assert
     assert sql == 'DROP TABLE event_store'
+
+
+def test_create_index_should_return_sql_create_index():
+    # arrange
+    name = 'event_store_id'
+    table = 'event_store'
+    column = 'id'
+    database_wrapper = DatabaseWrapper()
+    # act
+    sql = database_wrapper.create_index(
+        name=name,
+        table=table,
+        columns=[column],
+    )
+    # assert
+    assert sql == 'CREATE INDEX event_store_id ON event_store (id)'
