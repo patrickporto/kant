42cc773cf7b6f50b6332b7b40f5ea70f4acf3666
Patrick Porto <patrick.porto@stone.com.br>
2018-01-10 17:18:53 -0200

    Add method for drop table

--- kant/db/backends/postgresql.py
+++ kant/db/backends/postgresql.py
@@ -1,9 +1,14 @@
 class DatabaseWrapper(object):
     sql_create_table = 'CREATE TABLE {name}'
+    sql_drop_table = 'DROP TABLE {name}'
 
     def create_table(self, name):
         name = self.escape_string(name)
         return self.sql_create_table.format(name=name)
+
+    def drop_table(self, name):
+        name = self.escape_string(name)
+        return self.sql_drop_table.format(name=name)
 
     def escape_string(self, text):
         return text
--- tests/db/backends/postgresql/test_schema.py
+++ tests/db/backends/postgresql/test_schema.py
@@ -9,3 +9,13 @@ def test_create_table_should_return_sql_create_table():
     sql = database_wrapper.create_table(name=table_name)
     # assert
     assert sql == 'CREATE TABLE event_store'
+
+
+def test_drop_table_should_return_sql_drop_table():
+    # arrange
+    table_name = 'event_store'
+    database_wrapper = DatabaseWrapper()
+    # act
+    sql = database_wrapper.drop_table(name=table_name)
+    # assert
+    assert sql == 'DROP TABLE event_store'
