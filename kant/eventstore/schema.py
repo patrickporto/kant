af273973f322a584d4651d1c543666c679b1dc74
Patrick Porto <patrick.porto@stone.com.br>
2018-01-11 14:48:22 -0200

    Add drop table

--- kant/eventstore/schema.py
+++ kant/eventstore/schema.py
@@ -1,11 +1,19 @@
 def create_table(dbsession):
     with dbsession.cursor() as cursor:
         stmt = """
-        CREATE TABLE IF NOT EXISTS event_store (
+        CREATE TABLE event_store (
             id varchar(255),
             data json NOT NULL,
             created_at timestamp NOT NULL,
             updated_at timestamp NOT NULL
         )
+        """
+        cursor.execute(stmt)
+
+
+def drop_table(dbsession):
+    with dbsession.cursor() as cursor:
+        stmt = """
+        DROP TABLE event_store
         """
         cursor.execute(stmt)
--- tests/eventstore/test_schema.py
+++ tests/eventstore/test_schema.py
@@ -1,4 +1,4 @@
-from kant.eventstore.schema import create_table
+from kant.eventstore.schema import create_table, drop_table
 
 
 def test_create_table_should_create_table_if_not_exists(dbsession):
@@ -15,3 +15,21 @@ def test_create_table_should_create_table_if_not_exists(dbsession):
         """)
         (exists,) = cursor.fetchone()
         assert exists
+
+
+def test_drop_table_should_drop_table_if_exists(dbsession):
+    # arrange
+    create_table(dbsession)
+    # act
+    drop_table(dbsession)
+    # assert
+    with dbsession.cursor() as cursor:
+        cursor.execute("""
+        SELECT EXISTS (
+            SELECT 1
+            FROM information_schema.tables
+            WHERE table_name = 'event_store'
+        );
+        """)
+        (exists,) = cursor.fetchone()
+        assert not exists
