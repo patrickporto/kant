0bf7cd1d6e9417266ddb2d0e287e31565425cb3d
Patrick Porto <patrick.porto@stone.com.br>
2018-01-11 17:58:50 -0200

    Add package events for install

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 setup(
     name='kant',
-    version='0.1.2',
+    version='0.1.3',
     description='The CQRS and Event Sourcing framework for Python',
     url='http://github.com/patrickporto/kant',
     author='Patrick Porto',
     author_email='patrick.s.porto@gmail.com',
     license='MIT',
-    packages=['kant'],
+    packages=['kant', 'kant.events'],
     install_requires=[
         'sqlalchemy_utils',
         'python-dateutil',
