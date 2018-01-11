28ca4ee43272cb6f3ed76275a6ee2e8ad6323dfe
Patrick Porto <patrick.porto@stone.com.br>
2018-01-11 16:57:11 -0200

    Change version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='kant',
-    version='0.1.0',
+    version='0.1.2',
     description='The CQRS and Event Sourcing framework for Python',
     url='http://github.com/patrickporto/kant',
     author='Patrick Porto',
