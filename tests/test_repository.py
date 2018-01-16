cd9648ef7c08dd07987d95012c500aecc16f6ac8
Patrick Porto <patrick.porto@stone.com.br>
2018-01-16 15:04:23 -0200

    Refactory repository test

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -7,47 +7,42 @@ from kant.repositories import EventStoreRepository
 from kant.eventstore.schema import create_table
 
 
-class BankAccount(TrackedEntity):
-    def apply_bank_account_created(self, event):
-        self.id = event.id
-        self.owner = event.get('owner')
-
-
 @pytest.mark.asyncio
 async def test_save_should_create_event_store(dbsession):
     # arrange
     await create_table(dbsession)
-    bank_account_created = BankAccountCreated(
-        owner='John Doe'
-    )
+    entity_id = '052c21b6-aab9-4311-b954-518cd04f704c'
+    events = [
+        BankAccountCreated(
+            id=entity_id,
+            owner='John Doe'
+        )
+    ]
     # act
     async with dbsession.cursor() as cursor:
         event_store_repository = EventStoreRepository(cursor)
-        bank_account = BankAccount()
-        bank_account.dispatch(bank_account_created)
         last_version = await event_store_repository.save(
-            entity_id=bank_account.id,
-            events=bank_account.get_events(),
-            expected_version=bank_account.version,
+            entity_id=entity_id,
+            events=events,
         )
         # assert
         stmt = """
         SELECT event_store.id, event_store.data, event_store.created_at
         FROM event_store WHERE event_store.id = %(id)s
         """
-        await cursor.execute(stmt, {'id': bank_account.id})
+        await cursor.execute(stmt, {'id': entity_id})
         event_store_list = await cursor.fetchall()
         assert last_version == 0
         assert cursor.rowcount == 1
         # id
-        assert event_store_list[0][0] == bank_account_created.id
+        assert event_store_list[0][0] == entity_id
         # events
         assert len(event_store_list[0][1]) == 1
         # first event
         assert event_store_list[0][1][0]['$type'] == 'BankAccountCreated'
         assert event_store_list[0][1][0]['$version'] == 0
-        assert event_store_list[0][1][0]['id'] == bank_account_created.id
-        assert event_store_list[0][1][0]['owner'] == bank_account_created.owner
+        assert event_store_list[0][1][0]['id'] == entity_id
+        assert event_store_list[0][1][0]['owner'] == 'John Doe'
         # date created
         assert event_store_list[0][2] is not None
 
@@ -68,7 +63,7 @@ async def test_get_should_return_events(dbsession):
                 '$type': 'BankAccountCreated',
                 '$version': 0,
                 'id': entity_id,
-                'owner': 'Joe Doe',
+                'owner': 'John Doe',
             }]),
         })
         # act
@@ -79,14 +74,14 @@ async def test_get_should_return_events(dbsession):
         assert isinstance(stored_events[0], BankAccountCreated)
         assert stored_events[0].version == 0
         assert stored_events[0].id == entity_id
-        assert stored_events[0].owner == 'Joe Doe'
+        assert stored_events[0].owner == 'John Doe'
 
 
 @pytest.mark.asyncio
 async def test_get_should_raise_exception_when_not_found(dbsession):
     # arrange
     await create_table(dbsession)
-    entity_id = 'e0fbeecc-d68f-45b1-83c7-5cbc65f78af7',
+    entity_id = 'f2283f9d-9ed2-4385-a614-53805725cbac',
     async with dbsession.cursor() as cursor:
         event_store_repository = EventStoreRepository(cursor)
         # act and assert
