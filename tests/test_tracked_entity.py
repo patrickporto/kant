5ba5f9ffee53e09282b86890ee8161a891d68ea5
Patrick Porto <patrick.porto@stone.com.br>
2018-01-16 16:45:30 -0200

    Add tests for tracked entity

---
+++ tests/test_tracked_entity.py
@@ -0,0 +1,233 @@
+import pytest
+from fixtures import BankAccountCreated, DepositPerformed
+from kant.models import TrackedEntity
+
+
+@pytest.mark.asyncio
+async def test_tracked_entity_should_apply_one_event(dbsession):
+    # arrange
+    class BankAccount(TrackedEntity):
+        def apply_bank_account_created(self, event):
+            self.id = event.get('id')
+            self.owner = event.get('owner')
+            self.balance = 0
+
+    bank_account = BankAccount()
+    bank_account_created = BankAccountCreated(
+        id=123,
+        owner='John Doe',
+    )
+    # act
+    bank_account.dispatch(bank_account_created)
+    # assert
+    assert bank_account.version == -1
+    assert bank_account.current_version == 0
+    assert bank_account.id == 123
+    assert bank_account.owner == 'John Doe'
+    assert bank_account.balance == 0
+
+
+@pytest.mark.asyncio
+async def test_tracked_entity_should_apply_many_events(dbsession):
+    # arrange
+    class BankAccount(TrackedEntity):
+        def apply_bank_account_created(self, event):
+            self.id = event.get('id')
+            self.owner = event.get('owner')
+            self.balance = 0
+
+        def apply_deposit_performed(self, event):
+            self.balance += event.get('amount')
+
+    bank_account_created = BankAccountCreated(
+        id=123,
+        owner='John Doe',
+    )
+    deposit_performed = DepositPerformed(
+        amount=20,
+    )
+    # act
+    bank_account = BankAccount()
+    bank_account.dispatch(bank_account_created)
+    bank_account.dispatch(deposit_performed)
+    # assert
+    assert bank_account.version == -1
+    assert bank_account.current_version == 1
+    assert bank_account.id == 123
+    assert bank_account.owner == 'John Doe'
+    assert bank_account.balance == 20
+
+
+@pytest.mark.asyncio
+async def test_tracked_entity_should_load_events(dbsession):
+    # arrange
+    class BankAccount(TrackedEntity):
+        def apply_bank_account_created(self, event):
+            self.id = event.get('id')
+            self.owner = event.get('owner')
+            self.balance = 0
+
+        def apply_deposit_performed(self, event):
+            self.balance += event.get('amount')
+
+    events = [
+        BankAccountCreated(
+            id=123,
+            owner='John Doe',
+            version=0,
+        ),
+        DepositPerformed(
+            amount=20,
+            version=1,
+        ),
+        DepositPerformed(
+            amount=20,
+            version=2,
+        )
+    ]
+    # act
+    bank_account = BankAccount()
+    bank_account.fetch_events(events)
+    # assert
+    assert bank_account.version == 2
+    assert bank_account.current_version == 2
+    assert bank_account.id == 123
+    assert bank_account.owner == 'John Doe'
+    assert bank_account.balance == 40
+
+
+@pytest.mark.asyncio
+async def test_tracked_entity_should_apply_event_after_load_events(dbsession):
+    # arrange
+    class BankAccount(TrackedEntity):
+        def apply_bank_account_created(self, event):
+            self.id = event.get('id')
+            self.owner = event.get('owner')
+            self.balance = 0
+
+        def apply_deposit_performed(self, event):
+            self.balance += event.get('amount')
+
+    events = [
+        BankAccountCreated(
+            id=123,
+            owner='John Doe',
+            version=0,
+        )
+    ]
+    deposit_performed = DepositPerformed(
+        amount=20,
+    )
+    # act
+    bank_account = BankAccount()
+    bank_account.fetch_events(events)
+    bank_account.dispatch(deposit_performed)
+    # assert
+    assert bank_account.version == 0
+    assert bank_account.current_version == 1
+    assert bank_account.id == 123
+    assert bank_account.owner == 'John Doe'
+    assert bank_account.balance == 20
+
+
+@pytest.mark.asyncio
+async def test_tracked_entity_should_return_new_events(dbsession):
+    # arrange
+    class BankAccount(TrackedEntity):
+        def apply_bank_account_created(self, event):
+            self.id = event.get('id')
+            self.owner = event.get('owner')
+            self.balance = 0
+
+        def apply_deposit_performed(self, event):
+            self.balance += event.get('amount')
+
+    events = [
+        BankAccountCreated(
+            id=123,
+            owner='John Doe',
+            version=0,
+        )
+    ]
+    deposit_performed = DepositPerformed(
+        amount=20,
+    )
+    # act
+    bank_account = BankAccount()
+    bank_account.fetch_events(events)
+    bank_account.dispatch(deposit_performed)
+    result = bank_account.get_events()
+    # assert
+    assert len(result) == 1
+    assert result[0].version == 1
+    assert result[0].amount == 20
+
+
+@pytest.mark.asyncio
+async def test_tracked_entity_should_return_all_events(dbsession):
+    # arrange
+    class BankAccount(TrackedEntity):
+        def apply_bank_account_created(self, event):
+            self.id = event.get('id')
+            self.owner = event.get('owner')
+            self.balance = 0
+
+        def apply_deposit_performed(self, event):
+            self.balance += event.get('amount')
+
+    events = [
+        BankAccountCreated(
+            id=123,
+            owner='John Doe',
+            version=0,
+        )
+    ]
+    deposit_performed = DepositPerformed(
+        amount=20,
+    )
+    # act
+    bank_account = BankAccount()
+    bank_account.fetch_events(events)
+    bank_account.dispatch(deposit_performed)
+    result = bank_account.all_events()
+    # assert
+    assert len(result) == 2
+    assert result[0].version == 0
+    assert result[0].id == 123
+    assert result[0].owner == 'John Doe'
+    assert result[1].version == 1
+    assert result[1].amount == 20
+
+
+@pytest.mark.asyncio
+async def test_tracked_entity_should_return_stored_events(dbsession):
+    # arrange
+    class BankAccount(TrackedEntity):
+        def apply_bank_account_created(self, event):
+            self.id = event.get('id')
+            self.owner = event.get('owner')
+            self.balance = 0
+
+        def apply_deposit_performed(self, event):
+            self.balance += event.get('amount')
+
+    events = [
+        BankAccountCreated(
+            id=123,
+            owner='John Doe',
+            version=0,
+        )
+    ]
+    deposit_performed = DepositPerformed(
+        amount=20,
+    )
+    # act
+    bank_account = BankAccount()
+    bank_account.fetch_events(events)
+    bank_account.dispatch(deposit_performed)
+    result = bank_account.stored_events()
+    # assert
+    assert len(result) == 1
+    assert result[0].version == 0
+    assert result[0].id == 123
+    assert result[0].owner == 'John Doe'
