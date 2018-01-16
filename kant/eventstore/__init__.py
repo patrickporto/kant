2ecd29232cf0d63d5710d24f51b80b8ede5ca67b
Patrick Porto <patrick.porto@stone.com.br>
2018-01-16 14:55:05 -0200

    Remove event store connection

--- kant/eventstore/__init__.py
+++
@@ -1,2 +0,0 @@
-from .connection import *  # NOQA
-from .stream import *  # NOQA
--- kant/eventstore/connection.py
+++
@@ -1,7 +0,0 @@
-from .stream import StreamManager
-
-
-class EventStoreConnection(object):
-    def __init__(self, session):
-        self._session = session
-        self.streams = StreamManager(session)
