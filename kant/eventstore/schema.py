2263b0579785855815fe214f53d91397fae932f6
Patrick Porto <patrick.porto@stone.com.br>
2018-01-16 13:33:20 -0200

    Change field data from json to jsonb

--- kant/eventstore/schema.py
+++ kant/eventstore/schema.py
@@ -3,7 +3,7 @@ async def create_table(dbsession):
         stmt = """
         CREATE TABLE IF NOT EXISTS event_store (
             id varchar(255),
-            data json NOT NULL,
+            data jsonb NOT NULL,
             created_at timestamp NOT NULL,
             updated_at timestamp NOT NULL
         )
