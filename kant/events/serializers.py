9564c8e4baeb00d156b6418d72203aee4566a24b
Patrick Porto <patrick.porto@stone.com.br>
2018-01-18 17:18:23 -0200

    Add sort_keys in doctest

--- kant/events/serializers.py
+++ kant/events/serializers.py
@@ -8,8 +8,8 @@ class EventModelEncoder(JSONEncoder):
     >>> found_added = FoundAdded(amount=25.5)
     >>> isinstance(found_added, EventModel)
     True
-    >>> json.dumps(found_added, cls=EventModelEncoder)
-    '{"$version": 0, "amount": 25.5, "$type": "FoundAdded"}'
+    >>> json.dumps(found_added, cls=EventModelEncoder, sort_keys=True)
+    '{"$type": "FoundAdded", "$version": 0, "amount": 25.5}'
     """
     def default(self, obj):
         if isinstance(obj, EventModel):
