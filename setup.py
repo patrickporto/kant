e4fd3a3ff98a142cf0f0b854e458387fdf9556ae
Patrick Porto <patrick.porto@stone.com.br>
2018-01-19 13:22:52 -0200

    Add CPython in implementation list

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
     keywords='eventsourcing cqrs eventstore',
