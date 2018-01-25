c01daceffeae34c31934cbc5474e6d960ae0247d
Patrick Porto <patrick.porto@stone.com.br>
2018-01-25 14:30:10 -0200

    Add create and update in repository

--- kant/eventstore/repositories.py
+++ kant/eventstore/repositories.py
@@ -12,46 +12,56 @@ class EventStoreRepository:
     def __init__(self, session, *args, **kwargs):
         self.session = session
 
+    async def create(self, aggregate_id, events, expected_version=0):
+        events = events if isinstance(events, EventStream) else EventStream(events)
+        async with self.session.begin() as transaction:
+            stmt_select = """
+            SELECT event_store.data
+            FROM event_store WHERE event_store.id = %(id)s AND CAST(data ? '$version' AS INTEGER) >= %(version)s
+            FOR UPDATE
+            """
+            await self.session.execute(stmt_select, {
+                'id': str(aggregate_id),
+                'version': expected_version,
+            })
+            event_store = await self.session.fetchone()
+            if not event_store:
+                raise ObjectDoesNotExist()
+            stored_events = EventStream.loads(event_store[0])
+            if stored_events.version != expected_version:
+                raise ConsistencyError(
+                    current_version=events[-1]['version'],
+                    expected_version=expected_version,
+                    ours=events,
+                    theirs=stored_events,
+                )
+            stored_events += events
+            stmt_update = """
+            UPDATE event_store SET data=%(data)s, updated_at=NOW() WHERE id = %(id)s;
+            """
+            await self.session.execute(stmt_update, {
+                'id': str(aggregate_id),
+                'data': stored_events.dumps()
+            })
+
+    async def update(self, aggregate_id, events, expected_version=0):
+        events = events if isinstance(events, EventStream) else EventStream(events)
+        stmt = """
+        INSERT INTO event_store (id, data, created_at, updated_at)
+        VALUES (%(id)s, %(data)s, NOW(), NOW())
+        """
+        await self.session.execute(stmt, {
+            'id': str(aggregate_id),
+            'data': events.dumps()
+        })
+
     async def save(self, aggregate_id, events, expected_version=0):
         """ Save the events in the model """
-        async with self.session.begin() as transaction:
-            events = events if isinstance(events, EventStream) else EventStream(events)
-            try:
-                stmt_select = """
-                SELECT event_store.data
-                FROM event_store WHERE event_store.id = %(id)s AND CAST(data ? '$version' AS INTEGER) >= %(version)s
-                FOR UPDATE
-                """
-                await self.session.execute(stmt_select, {
-                    'id': str(aggregate_id),
-                    'version': expected_version,
-                })
-                event_store = await self.session.fetchone()
-                if not event_store:
-                    raise ObjectDoesNotExist()
-                stored_events = EventStream.loads(event_store[0])
-                if stored_events.version != expected_version:
-                    raise ConsistencyError(
-                        current_version=events[-1]['version'],
-                        expected_version=expected_version,
-                        ours=events,
-                        theirs=stored_events,
-                    )
-                entity_events = stored_events + events
-                stmt_save = """
-                UPDATE event_store SET data=%(data)s, updated_at=NOW() WHERE id = %(id)s;
-                """
-            except ObjectDoesNotExist:
-                entity_events = events
-                stmt_save = """
-                INSERT INTO event_store (id, data, created_at, updated_at)
-                VALUES (%(id)s, %(data)s, NOW(), NOW())
-                """
-            await self.session.execute(stmt_save, {
-                'id': str(aggregate_id),
-                'data': entity_events.dumps()
-            })
-            return entity_events.version
+        try:
+            await self.update(aggregate_id, events, expected_version)
+        except ObjectDoesNotExist:
+            await self.create(aggregate_id, events, expected_version)
+        return events[-1].version
 
     async def get(self, aggregate_id, initial_version=0):
         stmt = """
