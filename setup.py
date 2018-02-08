85c4d0f9e29c96799e650413d7dbf14fb0ea626d
Patrick Porto <patrick.porto@stone.com.br>
2018-02-08 20:52:59 -0200

    Update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(BASE_DIR, 'README.md')) as f:
 
 setup(
     name='kant',
-    version='1.1.2',
+    version='2.0.0',
     description='The CQRS and Event Sourcing framework for Python',
     url='http://github.com/patrickporto/kant',
     author='Patrick Porto',
