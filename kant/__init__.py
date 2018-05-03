7894b8c049a44123e51244b18f6adfa7925a5d88
Patrick Porto <patrick.porto@stone.com.br>
2018-05-03 19:31:46 -0300

    Add logging with NullHandler

--- kant/__init__.py
+++ kant/__init__.py
@@ -6,5 +6,10 @@
     :copyright: © 2018 by Patrick Porto.
     :license: MIT, see LICENSE for more details.
 """
+import logging
+
 
 __version__ = "3.0.0"
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
