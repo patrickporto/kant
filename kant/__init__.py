f1c4ee9af2882b34e8c711b5c512772488a068bc
Patrick Porto <patrick.porto@stone.com.br>
2018-05-03 19:25:30 -0300

    Add doc in __init__

--- kant/__init__.py
+++ kant/__init__.py
@@ -1,1 +1,10 @@
+"""
+    kant
+    ~~~~~
+     A CQRS and Event Sourcing framework.
+
+    :copyright: © 2018 by Patrick Porto.
+    :license: MIT, see LICENSE for more details.
+"""
+
 __version__ = "3.0.0"
